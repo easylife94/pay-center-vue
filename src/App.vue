167489d48f1c9b4883571200334c7dfe18e17928
chenwei <cw961590280@hotmail.com>
2019-01-10 14:43:18 +0800

    x

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,7 @@
 </template>
 <script>
 export default {
-  name: 'App',
-  data () {
-    return {
-      activeIndex: '1',
-      activeIndex2: '1'
-    }
-  },
-  methods: {
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleOpen (key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleClose (key, keyPath) {
-      console.log(key, keyPath)
-    }
-  }
+  name: 'App'
 }
 </script>
 
@@ -31,5 +14,4 @@ body {
   display: block;
   margin: 0px;
 }
-.el-header {background-color: rgb(84, 92, 100)}
 </style>
